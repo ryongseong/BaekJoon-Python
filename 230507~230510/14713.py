a35db1ff4ef6d80319c6b22ddb0ab5afda4e5c5c
ryongseong <xmssnsk2119@gmail.com>
2023-05-09 19:38:51 +0900

    python

--- 230507~230510/14713.py
+++ 230507~230510/14713.py
@@ -1,12 +1,38 @@
 from collections import deque
 
-aengmubird = int(input())
-sentence = deque(map(str, input().split()))
+parrot = int(input())
 word = []
 
-for _ in range(aengmubird):
-    word.append(deque(str, input().split()))
+for _ in range(parrot):
+    word.append(deque(map(str, input().split())))
+sentence = deque(map(str, input().split()))
 
-wordscount = 0
-wordcount = 0
+def result(sentence, n_bird):
+    misscount = 0
+    birdcount = 0
+    isfinish = 0
+    while sentence:
+        if n_bird[birdcount] and sentence[0] == n_bird[birdcount][0]:
+            n_bird[birdcount].popleft()
+            sentence.popleft()
+            misscount = 0
+        else:
+            if misscount == parrot:
+                return False
+            misscount += 1
+        birdcount = (birdcount+1)%parrot
+        
+    for i in range(parrot):
+        if len(n_bird[i]) == 0:
+            isfinish += 1
+    
+    if not sentence and isfinish == parrot:
+        return True
+    else:
+        return False
+
+if result(sentence, word):
+    print("Possible")
+else:
+    print("Impossible")
 
