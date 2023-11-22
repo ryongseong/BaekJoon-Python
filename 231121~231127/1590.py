f2019be517abde4075287b6d38fd23ce2afdac0c
ryongseong <xmssnsk2119@gmail.com>
2023-11-22 23:28:25 +0900

    Python

--- 231121~231127/1590.py
+++ 231121~231127/1590.py
@@ -1,2 +1,16 @@
 # https://www.acmicpc.net/problem/1590
+import sys
 
+N, T = map(int,input().split())
+
+Bus = []
+for i in range(N):
+    bus = list(map(int, input().split()))
+
+    if bus[0] <= T:
+        Bus.append(bus)
+
+for b in Bus:
+    if b[0] == T:
+        print(0)
+        break
--- 231121~231127/1769.py
+++ 231121~231127/1769.py
@@ -1,1 +1,14 @@
 # https://www.acmicpc.net/problem/1769 
+
+X = input()
+
+cnt = 0
+while int(X) >= 10:
+    Y = 0
+    for i in range(len(X)):
+        Y += int(X[i])
+    cnt += 1
+    X = str(Y)
+
+print(cnt)
+print("YES" if int(X)%3 == 0 else "NO")
