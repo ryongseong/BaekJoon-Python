ea9edb37301d2ed7c6ab6d5d6410b6d546abeaeb
ryongseong <ryongseong.dev@gmail.com>
2025-01-22 21:24:12 +0900

    Python

---
+++ Self-Study/1182.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+N, S = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+cnt = 0
+
+for i in range(1, N+1):
+    for j in combinations(arr, i):
+        if sum(j) == S:
+            cnt += 1
+
+print(cnt)
