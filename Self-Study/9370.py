0b145d0a1a3a066708f5d2a0122c9119fb9e89fd
ryongseong <ryongseong.dev@gmail.com>
2025-03-22 09:18:56 +0900

    Python

--- Self-Study/9370.py
+++ Self-Study/9370.py
@@ -1,23 +1,28 @@
 import sys
 from heapq import heappush, heappop
 
+sys.setrecursionlimit(10**6)
+
 input = sys.stdin.readline
+INF = int(1e9)
 
 
 def dijkstra(start_node):
-    pq = [(0, start_node, 0)]
-    dists = [[float("inf"), 0] for _ in range(n + 1)]
-    dists[start_node] = [0, 0]
+    pq = [(0, start_node)]
+    dists = [INF] * (n + 1)
+    dists[start_node] = 0
 
     while pq:
-        dist, node, cnt = heappop(pq)
-        if dists[node][0] < dist:
+        dist, node = heappop(pq)
+        if dists[node] < dist:
             continue
-
         for next_dist, next_node in graph[node]:
             new_dist = dist + next_dist
-            if new_dist < dists[next_node][0]:
-                dists[next_node] = [new_dist, dists[next_node][1] + 1]
+            if new_dist < dists[next_node]:
+                dists[next_node] = new_dist
+                heappush(pq, (new_dist, next_node))
+
+    return dists
 
 
 for _ in range(int(input())):
@@ -29,4 +34,15 @@ for _ in range(int(input())):
         graph[a].append((d, b))
         graph[b].append((d, a))
     destinations = [int(input()) for _ in range(t)]
-    dijkstra(s)
+    s_dijk = dijkstra(s)
+    g_dijk = dijkstra(g)
+    h_dijk = dijkstra(h)
+    result = []
+    for d in destinations:
+        if (
+            s_dijk[g] + g_dijk[h] + h_dijk[d] == s_dijk[d]
+            or s_dijk[h] + h_dijk[g] + g_dijk[d] == s_dijk[d]
+        ):
+            result.append(d)
+
+    print(*sorted(result))
