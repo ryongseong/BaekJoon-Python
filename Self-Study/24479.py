6a1dc3fa7ad9ac245e41c60320355d4ce5bd3e4a
ryongseong <xmssnsk2119@gmail.com>
2025-03-23 08:42:24 +0900

    Python

---
+++ Self-Study/24479.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+input = sys.stdin.readline
+
+def dfs(start):
+    global cnt
+    visited[start] = cnt
+    graph[start].sort()
+    for i in graph[start]:
+        if not visited[i]:
+            cnt += 1
+            dfs(i)
+
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+cnt = 1
+for _ in range(M):
+    a, b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+dfs(R)
+for i in range(1, N+1):
+    print(visited[i])
