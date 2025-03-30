7c3bc7d7ba8abeb61993180b4dfa83b16d929730
ryongseong <ryongseong.dev@gmail.com>
2025-03-30 10:35:33 +0900

    Python

--- Self-Study/_5719.py
+++ Self-Study/_5719.py
@@ -1,53 +1,63 @@
-from copy import deepcopy
 import sys
-from heapq import heappush, heappop
-
-input = sys.stdin.readline
+from collections import deque
 
 INF = int(1e9)
+input = sys.stdin.readline
 
 
-def dfs(node, cost, visited):
-    global min_cost
-
-    if cost > min_cost:
-        return
-
-    if node == D and min_cost != INF:
-        return visited
-
-
-def dijkstra():
-    pq = [(0, S)]
-    dists = [INF] * N
-    dists[S] = 0
-
-    while pq:
-        dist, node = heappop(pq)
-
-        if dists[node] < dist:
+def dijkstra(graph, costlist, s):
+    minload = [[] for x in range(n)]
+    queue = deque([(s, 0)])
+    costlist[s] = 0
+    while queue:
+        node, cost = queue.popleft()
+        if cost > costlist[node]:
             continue
+        for next_node, next_cost in graph[node]:
+            if next_cost != -1:
+                if costlist[next_node] > costlist[node] + next_cost:
+                    costlist[next_node] = costlist[node] + next_cost
+                    minload[next_node].clear()
+                    minload[next_node].append(node)
+                    queue.append((next_node, next_cost))
+                elif costlist[next_node] == costlist[node] + next_cost:
+                    minload[next_node].append(node)
+    return minload
 
-        for i in range(N):
-            pass
+
+def bfs(d, minload, visit, graph):
+    queue = deque([d])
+    visit[d] = True
+    while queue:
+        node = queue.popleft()
+        for next_node in minload[node]:
+            if visit[next_node] == False:
+                queue.append(next_node)
+                visit[next_node] = True
+            for index in range(len(graph[next_node])):
+                if graph[next_node][index][0] == node:
+                    graph[next_node][index][1] = -1
+    return graph
 
 
 while True:
-    N, M = map(int, input().split())
-    if N == M == 0:
+    n, m = map(int, input().split())
+    if n == 0 and m == 0:
         break
+    graph = [[] for x in range(n)]
+    s, d = map(int, input().split())
+    for i in range(m):
+        u, v, p = map(int, input().split())
+        graph[u].append([v, p])
+    costlist = [INF for x in range(n)]
 
-    graph = [[INF] * N for _ in range(N)]
-    S, D = map(int, input().split())
+    minload = dijkstra(graph, costlist, s)
+    visit = [False for _ in range(n)]
+    bfs(d, minload, visit, graph)
+    costlist = [INF for _ in range(n)]
+    dijkstra(graph, costlist, s)
 
-    min_cost = INF
-
-    for _ in range(M):
-        U, V, P = map(int, input().split())
-        graph[U][V] = P
-
-    second_graph = deepcopy(graph)
-
-    visited_path = dfs(S, 0, set())
-
-    print(second_graph)
+    if costlist[d] == INF:
+        print(-1)
+    else:
+        print(costlist[d])
